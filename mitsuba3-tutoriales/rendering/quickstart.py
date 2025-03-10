4107c249f1bb0a634b924e6241921e8b8c9435cc
alv02 <820800@unizar.es>
2025-03-10 14:21:43 +0100

    Cosas

--- mitsuba3-tutoriales/rendering/quickstart.py
+++ mitsuba3-tutoriales/rendering/quickstart.py
@@ -1,17 +1,19 @@
-import mitsuba as mi
+from typing import cast
+
 import matplotlib
-matplotlib.use('TkAgg')
+import mitsuba as mi
+
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
+mi.set_variant()
 
+mi.set_variant("llvm_ad_rgb")
 
-
-mi.set_variant('llvm_ad_rgb')
-
-scene = mi.load_file('../scenes/cbox.xml')
+scene = cast(mi.Scene, mi.load_file("../scenes/cbox.xml"))
 
 image = mi.render(scene, spp=256)
 
-plt.axis('off')
-plt.imshow(image ** (1.0/2.2))
+plt.axis("off")
+plt.imshow(image ** (1.0 / 2.2))
 plt.show()
